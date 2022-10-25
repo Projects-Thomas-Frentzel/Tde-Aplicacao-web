8109d9302f6fe9e548e3181e16e7345191b37b1b
Thomas <80831811+Thomas470@users.noreply.github.com>
2022-10-25 20:09:21 -0300

    Add files via upload

---
+++ Empresa/bd/conectaBD.php
@@ -0,0 +1,15 @@
+<?php
+global $servername ;
+global $username;
+global $password;
+global $database;
+
+$servername = "localhost:3307";
+$username = "usu@empresa";
+$password = "php@PUCPR";
+$database = "Empresa";
+?>
+
+
+
+
